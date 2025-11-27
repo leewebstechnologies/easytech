c1c0a58353cd3444717e558438faff04820dbb01
leewebstechnologies.ali <leewebstechnologies.aliuajibade317@gmail.com>
2025-11-27 00:35:01 -0800

    add case studies

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+// import "./globals.css";
+import "@/styles/style.scss";
+
+export const metadata = {
+  title: "EasyTech",
+  description: "IT and Payment Solutions",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import Topnav from "@/components/header/topnav/Topnav"
+import Menu from "@/components/header/menu/Menu"
+import Slider from "@/components/slider/Slider"
+import Service from "@/components/service/Service"
+import serviceData from "@/data/service.json";
+import PaymentGateway from "@/components/PaymentGateway/PaymentGateway";
+import CaseStudy from "../components/CaseStudy/CaseStudy";
+
+
+const Homepage = () => {
+  return (
+    <div className='overflow-x-hidden'>
+      <header id="header">
+        <Topnav />
+        <Menu />
+      </header>
+      <main className="content">
+        <Slider />
+        <Service data={serviceData}/>
+        <PaymentGateway />
+        <CaseStudy />
+      </main>     
+    </div>
+  )
+}
+
+export default Homepage
